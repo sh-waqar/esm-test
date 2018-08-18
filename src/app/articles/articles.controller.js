de54439f83ae1de1d4bd10ce4545aded72fbe393
Waqar Ali <waqar@adeven.com>
2018-08-18 11:56:53 +0200

    Add some service and wire it in articles controller

--- src/app/articles/articles.controller.js
+++ src/app/articles/articles.controller.js
@@ -1,8 +1,27 @@
-function ArticlesController() {
+function ArticlesController(someService) {
   'ngInject';
   var vm = this;
 
-  console.log('ar');
+  vm.count = 0
+  vm.numberA = 0;
+  vm.numberB = 0;
+
+  someService.setCount(vm.count);
+
+  vm.addNumbers = function() {
+    vm.count = articlesService.adds(vm.numberA, vm.numberB)
+  }
+
+  vm.addCount = function() {
+    someService.addCount()
+    vm.count = someService.getCount()
+  }
+
+  vm.subtractCount = function() {
+    someService.subtractCount()
+    vm.count = someService.getCount()
+  }
+
 }
 
 export { ArticlesController };
--- src/app/common/common.js
+++ src/app/common/common.js
@@ -1,5 +1,8 @@
 import angular from 'angular';
 
+import { someService } from './some.service'
+
 export default angular
   .module('esmTest.common', [])
+  .factory('someService', someService)
   .name;
